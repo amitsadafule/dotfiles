84d200f91781fdb12006c04c9b30de5742809f1f
amit sadafule <amit.sadafule@gmail.com>
2024-08-16 13:32:19 +0530

    fix issue with backspace

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ autocmd VimEnter * NERDTree
 " Exit Vim if NERDTree is the only window remaining in the only tab.
 autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | call feedkeys(":quit\<CR>:\<BS>") | endif
 
+set backspace=2
 let g:NERDTreeFileLines = 1
 let NERDTreeShowLineNumbers = 1
 au BufWinEnter * set number
